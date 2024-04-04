f4e94d942b2f5dc5d264023bbdc3b828f4c81ec8
bayleaflet <bayleejchristensen@gmail.com>
2024-04-04 14:11:16 -0600

    slurm script added

---
+++ bash_scripts/s.run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --account=utu
+#SBATCH --partition=lonepeak
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=1:00:00
+#SBATCH -o slurm-%j.out-%N
+#SBATCH -e slurm-%j.err-%N
+
+bash environment_setup.sh
