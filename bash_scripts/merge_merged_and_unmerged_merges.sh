db0d53bfe1cca184e68595ac14db362efafd7f97
bayleaflet <bayleejchristensen@gmail.com>
2024-04-23 14:51:40 -0600

    updated:

--- bash_scripts/merge_merged_and_unmerged_merges.sh
+++ bash_scripts/merge_merged_and_unmerged_merges.sh
@@ -2,7 +2,7 @@
 cd /scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/mapped_reads
 module load samtools
 for merged_read in *.bam; do
-	sample_name=$(echo $merged_read | cut -d "_" -f 1)
-	samtools merge $sample_name.bam ${sample_name}_unmerged_sorted.bam ${sample_name}_merged_sorted.bam
+	sample_name=$(echo $merged_read | grep -oE 'Am_[0-9]+')
+	samtools merge $sample_name.bam ${sample_name}_unmerged_*.bam ${sample_name}_merged*.bam
 done
 module unload samtools
