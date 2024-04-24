3af704b2d7fe1d3210e7f046dbd316f939413edf
syrusminer <syrus.miner@gmail.com>
2024-04-23 20:36:28 -0600

    Added the annotated Map_reads.sh, we decided to map using STAR instead of bwa mem.

--- bash_scripts/Map_reads.sh
+++ bash_scripts/Map_reads.sh
@@ -1,61 +1,103 @@
 #!/bin/bash
 
 {
-usage="$(basename "$0") [-h][-g <reference_genome>][-d <repository_directory>] [-w <working_directory>] [-t <number_of_processors>]"
+usage="$(basename "$0") [-h][-g <reference_genome>][-w <working_directory>] [-t <number_of_processors>]"
 usage+="\nThis program will trim and map merged reads from given SRA sequences files to compare mapping of different NGS datasets."
 usage+="\n    -h  show this help text"
-usage+="\n    -g  The reference genome (in fasta format). It is assumed that the genome is within the References directory in -d"
-usage+="\n    -d  Path to the repository directory (the path to the repository you cloned from github)"
 usage+="\n    -w  Path to the working directory (where processed files will be stored)"
 usage+="\n    -t  Number of CPU processors"
 
-options=':h:g:t:d:w:'
+options=':h:g:t:w:'
 while getopts $options option; do
   case "$option" in
     h) echo "$usage"; exit;;
-    g) g=$OPTARG;;
     t) t=$OPTARG;;
-    d) d=$OPTARG;;
     w) w=$OPTARG;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
     \?) printf "illegal option: -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
   esac
 done
 
-# Load necessary modules
-module load bwa/2020_03_19
+##########################
+# Load necessary modules #
+##########################
+
+module load gffread/0.12.7
+module load star/2.7.10a
 module load samtools/1.16
 
-# Index reference genome
+##########################
+# Index reference genome #
+##########################
+
 echo "Indexing Reference"
 echo ""
-cd "${d}"
-bwa index "${g}"
+cd "${w}"
+cd "${w}"/Reference
 
-#test
-echo "test1"
-# Align merged reads against reference
-echo "Aligning Merged Reads against Reference with bwa mem, using $t threads."
+###################################
+# convert gff to gtf for indexing #
+###################################
+
+# This can be passed over if the file is already a .gtf file.
+
+gffread -T a_marmoratus_AspMarm2.0_v1.gff -o a_marmoratus_AspMarm2.0_v1.gtf
+
+################
+# index genome #
+################
+
+STAR --runThreadN 8 --runMode genomeGenerate --genomeDir "${w}"/Reference --genomeFastaFiles "${g}" --sjdbGTFfile "${w}"/Reference/a_marmoratus_AspMarm2.0_v1.gtf --sjdbOverhang 100
+
+#############
+# map reads #
+#############
+
+# This creats .bam files for the merged and the unmerged reads, .sam files are not created. 
+# Our original samples were titled "Am_01_merged..."
+
+echo "Aligning Merged Reads against Reference with STAR, using $t threads."
 echo ""
 
-cd "${w}/cleaned_reads/merged_reads"
+cd "${w}"/cleaned_reads/merged_reads
 for merged_read in *.fastq.gz; do
     sample_name=$(echo $merged_read | cut -d "_" -f "1,2" )
-    bwa mem "${g}" $merged_read > "${w}/mapped_reads/${sample_name}_merged_mapped.sam"
-    bwa mem "${g}" ../unmerged_reads/"${sample_name}"_unmerged1.fastq ../unmerged_reads/"${sample_name}"_unmerged2.fastq > "${w}/mapped_reads/${sample_name}_unmerged_mapped.sam"
-    samtools sort "${w}/mapped_reads/${sample_name}_merged_mapped.sam" > "${w}/mapped_reads/${sample_name}_merged_sorted.bam" -@ "${t}"
-    samtools sort "${w}/mapped_reads/${sample_name}_unmerged_mapped.sam" > "${w}/mapped_reads/${sample_name}_unmerged_sorted.bam" -@ "${t}"
-done
-#test2
-echo "test2"
-# Unload modules
-module unload bwa/2020_03_19
-module unload samtools/1.16
 
-# Done
+# map merged clean reads
+STAR --genomeDir "${w}"/Reference \
+      --runThreadN 8 \
+      --readFilesIn  "${w}"/cleaned_reads/merged_reads/$merged_read \
+      --readFilesCommand zcat \
+      --outSAMtype BAM SortedByCoordinate \
+      --quantMode GeneCounts \
+      --outFileNamePrefix "${w}"/mapped_reads/${sample_name}_merged_
+
+# map unmerged clean reads
+STAR --genomeDir "${w}"/Reference \
+      --runThreadN 8 \
+      --readFilesIn  "${w}"/cleaned_reads/unmerged_reads/${sample_name}_unmerged1.fastq \
+                     "${w}"/cleaned_reads/unmerged_reads/${sample_name}_unmerged1.fastq \
+      --readFilesCommand zcat \
+      --outSAMtype BAM SortedByCoordinate \
+      --quantMode GeneCounts \
+      --outFileNamePrefix "${w}"/mapped_reads/${sample_name}_unmerged_
+
+done
+
+##################
+# Unload modules #
+##################
+
+module unload star/2.7.10a
+module unload samtools/1.16
+module unload gffread/0.12.7
+
 echo "Alignment completed successfully."
 
-# Record execution stats
+##########################
+# Record execution stats #
+##########################
+
 end=$(date +%s)
 elapsed=$((end - begin))
 echo "Time taken: $elapsed"
