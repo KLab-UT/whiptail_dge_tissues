e25b3e69a17028095f6a2f09d7b5557f7dbda7d2
syrusminer <syrus.miner@gmail.com>
2024-04-23 20:38:51 -0600

    Added environment_setup.sh

--- bash_scripts/environment_setup.sh
+++ bash_scripts/environment_setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+#####################
+# Environment Setup #
+#####################
+
+# cd into the desired working directory, this will be {w} that is used in Map_reads.sh
+
 cd /scratch/general/nfs1/utu_4310/whiptail_dge_working_directory
 
 mkdir -p cleaned_reads
