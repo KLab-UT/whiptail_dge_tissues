1507e693843b0e068778fc3c4a219ac52351373c
syrusminer <syrus.miner@gmail.com>
2024-04-23 21:06:23 -0600

    Added all of the updated files

--- bash_scripts/new_trim_rna_reads.sh
+++ bash_scripts/new_trim_rna_reads.sh
@@ -4,9 +4,18 @@
 ## Move all of the raw reads from this directory into $d/raw_reads
 ## MOVE not COPY
 
+##############################
+# load all necessary modules #
+##############################
+
+module load fastqc/0.11.4
+module load fastp/0.20.1
+
 echo ""
 echo "Beginning script"
 echo ""
+
+# Usage information for the script
 usage="$(basename "$0") [-h] [-d <working_directory>]
 Script to perform raw read preprocessing using fastp
     -h show this help text
@@ -14,18 +23,22 @@ Script to perform raw read preprocessing using fastp
 options=':h:l:d:'
 while getopts $options option; do
     case "$option" in
-        h) echo "$usage"; exit;;
-	d) d=$OPTARG;;
+        h) echo "$usage"; exit;;    # Display usage information
+	d) d=$OPTARG;;              # Set working directory
 	:) printf "missing argument for -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
        \?) printf "illegal option: -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
      esac
 done
 
 
-begin=`date +%s`
+begin=`date +%s` # record start time
 
 echo "copy over raw reads for this project"
 cd /scratch/general/nfs1/utu_4310/whiptail_shared_data/raw_rna_reads/01.RawData
+
+# Copy raw reads to the specified working directory. Our data was Am_01-15 and Am_25-27.
+# This can be modified as needed.
+
 for i in {01..15}
 do
 	cp Am_$i/*.gz $d/raw_reads
@@ -36,28 +49,26 @@ do
 done
 
 
-echo "load required modules"
-echo ""
-module load fastqc/0.11.4
-# fastqc loads the html website
-module load fastp/0.20.1
-
 echo "create file storing environment"
 echo ""
 
 ###################################
 # Quality check of raw read files #
 ###################################
+
 echo "Perform quality check of raw read files\n"
 echo ""
 
+# Move to the directory containing the raw reads, should be created in {w} environment_setup.sh
 cd /scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/raw_reads
 
+# Create a list of sample names
 ls *.fq.gz | cut -d "_" -f 1,2  | uniq > sample_list.txt
 
+# Perform FastQC on each sample
 while read i; do 
-  	fastqc "$i"_1.fq.gz # insert description here
-  	fastqc "$i"_2.fq.gz # insert description here
+  	fastqc "$i"_1.fq.gz 
+  	fastqc "$i"_2.fq.gz 
 done<sample_list.txt
 
 ####################################################
@@ -72,8 +83,8 @@ fastp -i "$z"_1.fq.gz -I "$z"_2.fq.gz \
       --out1 ${d}/cleaned_reads/unmerged_reads/"$z"_unmerged1.fastq --out2 ${d}/cleaned_reads/unmerged_reads/"$z"_unmerged2.fastq \
       -e 25 -q 15 \
       -u 40 -l 15 \
-      --adapter_sequence AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \
-      --adapter_sequence_r2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
+      --adapter_sequence AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \     # These are the adapter_sequences used by Illumina
+      --adapter_sequence_r2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \  # ie. Illumina data was used
       -M 20 -W 4 -5 -3 \
       -c 
 cd ../cleaned_reads/merged_reads
@@ -93,7 +104,7 @@ echo "Perform check of cleaned read files"
 cd ${d}/cleaned_reads/merged_reads
 pwd
 while read i; do 
-	fastqc "$i"_merged.fastq.gz # insert description here
+	fastqc "$i"_merged.fastq.gz 
 done<${d}/raw_reads
 
 
