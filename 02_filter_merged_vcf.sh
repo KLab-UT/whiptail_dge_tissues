cf121827dab121d7c23c40393eaf1468379922de
bayleaflet <bayleejchristensen@gmail.com>
2024-09-10 14:44:39 -0600

    Pushing non-functioning script 02 to repo, adjustments to be made

--- 02_filter_merged_vcf.sh
+++ 02_filter_merged_vcf.sh
@@ -22,6 +22,8 @@ GFF_FILE_PATH=/uufs/chpc.utah.edu/common/home/u6057891/whiptail_dge_tissues/MiCF
 
 module load bedops/2.4.41
 module load htslib/1.18
+module load bcftools/1.16
+module load vcftools
 
 attr_filtration_gff() {
     local gff_file=$GFF_FILE_PATH
@@ -59,5 +61,42 @@ attr_filtration_gff() {
 # Call function
 attr_filtration_gff
 
+
+hard_variant_filtration {
+  # Step 1: Get rid of low-quality (mean) genotyping:
+    vcftools --vcf "$1".vcf --out "$2"_HardFilterStep1 --minGQ 20 --recode --recode-INFO-all
+    mv "$2"_HardFilterStep1.recode.vcf "$2"_HardFilterStep1.vcf
+    echo "Post genotype Quality filtration $2 variants: $(grep -v "^#" "$2"_HardFilterStep1.vcf | wc -l)" >> Log.txt
+  # Step 2: Get rid of low-depth individuals per site
+    vcftools --vcf "$2"_HardFilterStep1.vcf --out "$2"_HardFilterStep2 --minDP 10 --recode --recode-INFO-all
+    mv "$2"_HardFilterStep2.recode.vcf "$2"_HardFilterStep2.vcf
+    echo "Post depth filtration $2 variants: $(grep -v "^#" "$2"_HardFilterStep2.vcf | wc -l)" >> Log.txt
+  # Step 3: Get rid of multiallelic SNPs (more than 2 alleles):
+    bcftools view -m2 -M2 -v snps "$2"_HardFilterStep3.vcf > "$2"_HardFilterStep4.vcf
+    echo "Post multiallelic filtration $2 variants: $(grep -v "^#" "$2"_HardFilterStep4.vcf | wc -l)" >> Log.txt
+  # Step 4: Get rid of low-frequency alleles- here just singletons:
+    vcftools --mac 2 --vcf "$2"_HardFilterStep4.vcf --recode --recode-INFO-all --out "$2"_HardFilterStep5
+    mv "$2"_HardFilterStep5.recode.vcf "$2"_HardFilterStep5.vcf
+    echo "Post singleton filtration $2 variants: $(grep -v "^#" "$2"_HardFilterStep5.vcf | wc -l)" >> Log.txt
+  # Step 5: Remove sites missing high amounts of data
+    vcftools --max-missing 0.3 --vcf "$2"_HardFilterStep5.vcf --recode --recode-INFO-all --out "$2"_HardFilterStep6
+    mv "$2"_HardFilterStep6.recode.vcf "$2"_HardFilterStep6.vcf
+    echo "Post removal of sites with high missing data: $(grep -v "^#" "$2"_HardFilterStep6.vcf | wc -l)" >> Log.txt
+  # Finishing: Compress data
+    bgzip "$2"_HardFilterStep6.vcf
+    bcftools index -f "$2"_HardFilterStep6.vcf.gz
+  # Move all stuff to it's own directory
+    mkdir -p hard_filter_files
+    mv *HardFilter* hard_filter_files
+
+}
+
+
+# Call function
+#attr_filtration_gff
+hard_variant_filtration $VCF_FILE_PATH
+
 module unload bedops/2.4.41
 module unload htslib/1.18
+module unload bcftools/1.16
+module unload vcftools
