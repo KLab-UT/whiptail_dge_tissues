3ab492be7f85f08e2cd92bfae4aa95c3a3510e2f
bayleaflet <bayleejchristensen@gmail.com>
2024-10-11 11:15:57 -0600

    Adding files for biogrid fetch

--- 06_isolate_mito_genes_from_web.sh
+++ 06_isolate_mito_genes_from_web.sh
@@ -1,44 +1,12 @@
 #!/bin/bash
 
-# Grabbed the mitochondrial genes from this website: https://www.ncbi.nlm.nih.gov/gene/?term=Homo+sapiens+mitochondrial
 
 
-
-INPUT_FILE=/scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/gene_result.txt
-# Sort to only have homo sapiens (for some reason all were grabbed)
-
-#awk -F'\t' '$2 ~ /Homo/' $INPUT_FILE> filtered_output.txt
-
-FIRST_FILE=/scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/filtered_output.txt
-SECOND_FILE=/scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/matrix/combined_variants.csv
-OUT_DIR=/scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/
-
-
-
-#awk -F',' -v second_file="$FIRST_FILE" '
-#    BEGIN {
-#        OFS = ","; 
-#        # Read second file into an array
-#        while (getline < second_file) {
-#            if (NR > 1) { 
-#                gene = $4; 
-#                data[gene] = $0;  # Store all columns for each gene in the array
-#            }
-#        }
-#    }
-    # Process the first file and check if the gene from column 7 matches the gene in second file
-#    {
-#        gene = $7; 
-#        if (gene in data) {
-#            print $0, data[gene];  # Print the matching row from both files
-#        }
-#    }
-#' $SECOND_FILE > ${OUT_DIR}/spliced_output.csv
-
-
-
-# Control V, down or up as many lines as you want, then shift i '#'
-
-
+VARIANT_PATH=/scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/matrix/rd_mito_variants_updated.csv
+MITO_GENE_PATH=/scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/matrix/37_mito_genes.csv
 # Grab only unique genes from mito from rd_mito_variants_updated.csv
-cut -d',' -f4 your_file.txt | sort | uniq | paste -sd ',' > unique_gene_list.txt
+cut -d',' -f4 $VARIANT_PATH | sort | uniq | paste -sd ',' > unique_gene_list.txt
+
+# Format 37 mito genes so in appropriate format for gene interaction API
+awk 'NR > 1 { sub(/^MT-/, "", $1); print "\"" $1 "\"" }' $MITO_GENE_PATH | paste -sd, - > 37_mito_genes.txt
+
