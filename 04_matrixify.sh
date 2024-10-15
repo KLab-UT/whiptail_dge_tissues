59eaeeafa404f4bac1736df8e13fe9802945dfb5
bayleaflet <bayleejchristensen@gmail.com>
2024-10-15 14:44:04 -0600

    Further documented one of the scripts

--- 04_matrixify.sh
+++ 04_matrixify.sh
@@ -10,12 +10,15 @@
 
 # Author: Baylee Christensen
 # Date: 09/13/2024
-# Description: 
-# Usage for cmd line: 
+# Description: This function creates a data table for the variants that have a
+# gene match and no gene match. The headers are:
+# "SampleID","Chromosome","Position","Gene","RefAD","AltAD","DeltaAD","GT.PL.DP.SP.AD","FieldTag","Tissue","GeneType"
+# Depending on if your file has matches for genes or not, you will choose the
+# format type of 'match' or 'no_match'
 
-# Usage for filetype with gene matches in sbatch: 
+# Usage for filetype with gene matches in sbatch:
 # sbatch 04_matrixify.sh -v /scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/spliced_vcfs/rd_mito_gene_matches.vcf -o /scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/matrix/rd_mito_variants.csv -f gene_match
-# Usage for filetype with gene matches in sbatch: 
+# Usage for filetype with gene matches in sbatch:
 # sbatch 04_matrixify.sh -v /scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/spliced_vcfs/rd_no_mito_gene_matches.vcf -o /scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/matrix/rd_no_mito_variants.csv -f no_match
 
 PY_SCRIPT_PATH=/uufs/chpc.utah.edu/common/home/u6057891/whiptail_dge_tissues
@@ -32,7 +35,7 @@ do
 done
 
 # Calls python script to output the combined matrix
-python3 ${PY_SCRIPT_PATH}/04_p_matrixify.py -i "$VCF_BED_FILE_PATH" -o "$OUTPUT_FILE_PATH" -f "$FORMAT_TYPE"	 
+python3 ${PY_SCRIPT_PATH}/04_p_matrixify.py -i "$VCF_BED_FILE_PATH" -o "$OUTPUT_FILE_PATH" -f "$FORMAT_TYPE"
 
 
 # Ensure Python script ran successfully
@@ -48,7 +51,6 @@ if [[ $? -ne 0 ]]; then
     echo "R script failed. Exiting."
     exit 1
 fi
-
 
 
 
