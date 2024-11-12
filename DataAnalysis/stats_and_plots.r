7303b1220d04a4edd03e68881053c43318868d20
bayleaflet <bayleejchristensen@gmail.com>
2024-11-12 12:57:11 -0700

    Completed analysis, updated R script

--- DataAnalysis/stats_and_plots.r
+++ DataAnalysis/stats_and_plots.r
@@ -1,19 +1,8 @@
-library(ggplot2)
-
-setwd('/Users/r_klabacka/OneDrive - Utah Tech University/KLab/Research/2023_WhiptailNmtVariation/whiptail_dge_tissues_data')
-
-dat_all <- read.csv('all_variants.csv')
-
-dat_mt <- read.csv('rd_mito_variants_updated.csv')
-dat_mt_sites_withdups <- dat_mt %>% select(Chromosome, Position, RefAD, AltAD, DeltaAD, GeneType)
-dat_mt_sites <- dat_mt_sites_withdups %>% distinct()
-
-dat_nonmt <- read.csv('rd_no_mito_variants_updated.csv')
-dat_nonmt_sites_withdups <-  dat_nonmt %>% select(Chromosome, Position, RefAD, AltAD, DeltaAD, GeneType)
-dat_nonmt_sites <- dat_nonmt_sites_withdups %>% distinct()
-dat_nonmt_sites$GeneType <- "NonMito"
-
-dat <- rbind(dat_mt_sites, dat_nonmt_sites)
+library('ggplot2')
+library('dplyr')
+# Adjust path as needed
+setwd('~/Library/CloudStorage/OneDrive-UtahTechUniversity/School/Research/whiptail_dge_tissues')
+dat <- read.csv("combined_variants.csv.gz")
 
 lm_results <- lm(DeltaAD~GeneType, data = dat)
 
@@ -31,8 +20,9 @@ dat_boxplots <- ggplot(dat, aes(x = GeneType, y = DeltaAD)) +
     axis.title.x = element_text(size = 1.5 * 11),  # Increase x-axis title font size
     plot.title = element_text(size = 1.5 * 14)     # Increase plot title font size by 50%
   )
+dat_boxplots
 
-ggsave("Figures/dat_boxplots.png", plot = dat_boxplots, width = 6, height = 8, dpi = 300)
+ggsave("3_MITO_DGE/Figures/dat_boxplots.png", plot = dat_boxplots, width = 6, height = 8, dpi = 300)
 
 dat_jitterplots <- ggplot(dat, aes(x = GeneType, y = DeltaAD)) +
   geom_jitter(color = "#00D8C4", width = 0.2, size = 2) +  # Jitter plot with custom color and size
@@ -48,10 +38,12 @@ dat_jitterplots <- ggplot(dat, aes(x = GeneType, y = DeltaAD)) +
     plot.title = element_text(size = 1.5 * 14)     # Increase plot title font size by 50%
   )
 
-ggsave("Figures/dat_jitterplots.png", plot = dat_jitterplots, width = 6, height = 8, dpi = 300)
+dat_jitterplots
+ggsave("3_MITO_DGE/Figures/dat_jitterplots.png", plot = dat_jitterplots, width = 6, height = 8, dpi = 300)
+
 
 dat_Mt <- dat[dat$GeneType %in% c("Mito"), ]
-dat_NonMt <- dat[dat$GeneType %in% c("Non-Mito"), ]
+dat_NonMt <- dat[dat$GeneType %in% c("Other"), ]
 
 dat_Mt_distributions <- ggplot(dat_Mt) +
   geom_density(aes(x = log(RefAD), fill = "RefAD"), color = "black", alpha = 0.5) +
@@ -59,7 +51,7 @@ dat_Mt_distributions <- ggplot(dat_Mt) +
   scale_fill_manual(values = c("RefAD" = "#00D8C4", "AltAD" = "#FFE812")) +  # Custom colors
   labs(title = "Overlay of Mito RefAD and AltAD Distributions", x = "Value", y = "Density", fill = "Legend") +
   theme_classic() +
-    theme(
+  theme(
     legend.position = "none",        # Remove the legend
     axis.title.y = element_text(size = 1.5 * 11),  # Increase y-axis title font size by 50%
     axis.text = element_text(size = 1.5 * 11),     # Increase axis text font size by 50%
@@ -67,7 +59,7 @@ dat_Mt_distributions <- ggplot(dat_Mt) +
     plot.title = element_text(size = 1.5 * 14)     # Increase plot title font size by 50%, if there's a title
   )
 
-ggsave("Figures/dat_Mt_distributions.png", plot = dat_Mt_distributions, width = 6, height = 8, dpi = 300)
+ggsave("3_MITO_DGE/Figures/dat_Mt_distributions.png", plot = dat_Mt_distributions, width = 6, height = 8, dpi = 300)
 
 dat_Mt_distributions <- ggplot(dat_NonMt) +
   geom_density(aes(x = log(RefAD), fill = "RefAD"), color = "black", alpha = 0.5) +
@@ -75,7 +67,7 @@ dat_Mt_distributions <- ggplot(dat_NonMt) +
   scale_fill_manual(values = c("RefAD" = "#00D8C4", "AltAD" = "#FFE812")) +  # Custom colors
   labs(title = "Overlay of Non-Mito RefAD and AltAD Distributions", x = "Value", y = "Density", fill = "Legend") +
   theme_classic() +
-    theme(
+  theme(
     legend.position = "none",        # Remove the legend
     axis.title.y = element_text(size = 1.5 * 11),  # Increase y-axis title font size by 50%
     axis.text = element_text(size = 1.5 * 11),     # Increase axis text font size by 50%
@@ -83,4 +75,24 @@ dat_Mt_distributions <- ggplot(dat_NonMt) +
     plot.title = element_text(size = 1.5 * 14)     # Increase plot title font size by 50%, if there's a title
   )
 
-ggsave("Figures/dat_NonMt_distributions.png", plot = dat_Mt_distributions, width = 6, height = 8, dpi = 300)
+ggsave("3_MITO_DGE/Figures/dat_NonMt_distributions.png", plot = dat_Mt_distributions, width = 6, height = 8, dpi = 300)
+
+
+dat_tissue_boxplots <- ggplot(dat, aes(x = Tissue, y = DeltaAD)) +
+  geom_boxplot(outlier.shape = NA, fill = "#00D8C4") +  # Add boxplot without outliers
+  labs(title = "Boxplot with Distribution of DeltaAD by Tissue",
+       x = "Tissue",
+       y = "Allele-biased Expression") +
+  coord_cartesian(ylim = c(-200, 250)) +  # Set minimum y-axis limit to -10000
+  theme_classic() +
+  theme(
+    legend.position = "none",        # Remove the legend
+    axis.title.y = element_text(size = 1.5 * 11),  # Increase y-axis title font size by 50%
+    axis.text = element_text(size = 1.5 * 11),     # Increase axis text font size by 50%
+    axis.title.x = element_text(size = 1.5 * 11),  # Increase x-axis title font size
+    plot.title = element_text(size = 1.5 * 14)     # Increase plot title font size by 50%
+  )
+dat_tissue_boxplots
+ggsave("3_MITO_DGE/Figures/dat_tissue_boxplots.png", plot = dat_tissue_boxplots, width = 6, height = 8, dpi = 300)
+
+
