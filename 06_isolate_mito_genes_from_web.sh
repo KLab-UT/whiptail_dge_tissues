cf0509f415a3200cf4169dc573a38852fb136778
bayleaflet <bayleejchristensen@gmail.com>
2024-11-15 09:26:29 -0700

    Adjusted script to call correct NCBI file, and grab correct column

--- 06_isolate_mito_genes_from_web.sh
+++ 06_isolate_mito_genes_from_web.sh
@@ -13,10 +13,10 @@
 # some formatting for the mito-mito interacting genes
 
 
-VARIANT_PATH=/scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/matrix/rd_mito_variants_updated.csv
+VARIANT_PATH=/scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/matrix/rd_mito_variants_NCBI_symbol.csv
 MITO_GENE_PATH=/scratch/general/nfs1/utu_4310/whiptail_dge_working_directory/matrix/37_mito_genes.csv
 # Grab only unique genes from mito from rd_mito_variants_updated.csv
-cut -d',' -f4 $VARIANT_PATH | sort | uniq | paste -sd ',' > unique_gene_list.txt
+cut -d',' -f12 $VARIANT_PATH | sort | uniq | paste -sd ',' > unique_gene_list.txt
 
 # Format 37 mito genes so in appropriate format for gene interaction API
 awk 'NR > 1 { print "\"" $1 "\"" }' $MITO_GENE_PATH | paste -sd, - > 37_mito_genes.txt
